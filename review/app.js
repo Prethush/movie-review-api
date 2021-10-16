b49c397e077dbe31b78180eb977016b05f0d5331
Prethush <ktmirash49@.gmail.com>
2021-10-16 11:23:27 +0530

    added files

--- review/app.js
+++ review/app.js
@@ -25,6 +25,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res, next) => {
+  res.send("Hello");
+});
 app.use('/api/v1/', indexRouter);
 app.use('/api/v1/users/', usersRouter);
 
